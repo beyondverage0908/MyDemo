fa6c7ade7047b0cf6049d01106e2184c745cee08
linpingjun <linpingjun@ihefe.com>
2016-05-03 19:51:50 +0800

    commit - 光栅化

--- UIPerformanceTestDemo/UIPerformanceTestDemo/SirTableViewCell.swift
+++ UIPerformanceTestDemo/UIPerformanceTestDemo/SirTableViewCell.swift
@@ -22,14 +22,17 @@ class SirTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        //demoLabel.opaque = true
+        demoLabel.opaque = true
         //demoLabel.backgroundColor = UIColor.whiteColor()
-        //demoLabel.layer.shouldRasterize = true //开启光栅化操作，用于将图层转化为bitmap，用于缓存策略
+        demoLabel.layer.shouldRasterize = true //开启光栅化操作，用于将图层转化为bitmap，用于缓存策略
         
         headerImageView.layer.shadowColor = UIColor.blackColor().CGColor
         headerImageView.layer.shadowRadius = 2
         headerImageView.layer.shadowOpacity = 1
         headerImageView.layer.shadowOffset = CGSizeMake(2, 1)
+        headerImageView.backgroundColor = UIColor.whiteColor()
+        headerImageView.layer.shouldRasterize = true
+        headerImageView.layer.rasterizationScale = UIScreen.mainScreen().scale
 //        headerImageView.layer.shadowPath = UIBezierPath(rect: headerImageView.bounds).CGPath // 指定阴影曲线，防止阴影效果带来的离屏渲染
         
     }
