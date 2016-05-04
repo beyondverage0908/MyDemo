989041c0f69eab1e77659c64c33bfe6ead1fdd0b
linpingjun <linpingjun@ihefe.com>
2016-05-04 17:05:34 +0800

    commit - 优化cell

--- UIPerformanceTestDemo/UIPerformanceTestDemo/SirTableViewCell.swift
+++ UIPerformanceTestDemo/UIPerformanceTestDemo/SirTableViewCell.swift
@@ -23,8 +23,13 @@ class SirTableViewCell: UITableViewCell {
         super.awakeFromNib()
         
         demoLabel.opaque = true
-        //demoLabel.backgroundColor = UIColor.whiteColor()
-        demoLabel.layer.shouldRasterize = true //开启光栅化操作，用于将图层转化为bitmap，用于缓存策略
+        demoLabel.backgroundColor = UIColor.whiteColor()
+//        demoLabel.layer.shouldRasterize = true //开启光栅化操作，用于将图层转化为bitmap，用于缓存策略
+        
+//        demoLabel.layer.cornerRadius = 10 // 圆角
+//        demoLabel.clipsToBounds = true
+//        demoLabel.layer.masksToBounds = true
+//        demoLabel.backgroundColor = UIColor.purpleColor()
         
         headerImageView.layer.shadowColor = UIColor.blackColor().CGColor
         headerImageView.layer.shadowRadius = 2
@@ -33,7 +38,7 @@ class SirTableViewCell: UITableViewCell {
         headerImageView.backgroundColor = UIColor.whiteColor()
         headerImageView.layer.shouldRasterize = true
         headerImageView.layer.rasterizationScale = UIScreen.mainScreen().scale
-//        headerImageView.layer.shadowPath = UIBezierPath(rect: headerImageView.bounds).CGPath // 指定阴影曲线，防止阴影效果带来的离屏渲染
+        headerImageView.layer.shadowPath = UIBezierPath(rect: headerImageView.bounds).CGPath // 指定阴影曲线，防止阴影效果带来的离屏渲染
         
     }
 
